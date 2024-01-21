b806052a43c7ca353dffe60cde20ac7397de6e92
Michael J Salgado <92074543+earthhworms@users.noreply.github.com>
2024-01-21 06:57:42 -0500

    Update FinanceProject.py

--- Python/FinanceProject.py
+++ Python/FinanceProject.py
@@ -6,7 +6,7 @@
 # In this data project we will focus on exploratory data analysis of stock prices.
 # We'll focus on bank stocks and see how they progressed throughout the [financial crisis](https://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%9308) all the way to early 2016.
 
-# TO SEE A MORE DETAILED VIEW, SEE 
+# TO SEE A MORE DETAILED VIEW, INCLUDING GRAPHS SEE 
 ' https://salgadomichael.com/finance-project '
 
 # ## Getting the Data
